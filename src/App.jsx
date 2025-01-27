31a28f266a73cfe62587097ce994b3dff85d32c9
Aldrian23 <aldrian.nieto.samson@gmail.com>
2025-01-28 01:18:22 +0800

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "./layout/MainLayout";
+
+const App = () => {
+  return (
+    <MainLayout>
+      <h1 className="text-center text-main">Main App</h1>
+    </MainLayout>
+  );
+};
+
+export default App;
