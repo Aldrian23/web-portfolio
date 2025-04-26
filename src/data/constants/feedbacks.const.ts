bbab3a117190868a9e942471501033b7e14a5805
Aldrian23 <aldrian.nieto.samson@gmail.com>
2025-04-26 20:56:30 +0800

    feedbacks

--- src/data/constants/feedbacks.const.ts
+++ src/data/constants/feedbacks.const.ts
@@ -6,16 +6,34 @@ export const FEEDBACKS = {
     ],
     list: [
         {
-            name: "Jessica Santos",
-            relation: "Classmate",
-            createdAt: moment("2025-02-12 14:42:12").toDate(),
-            value: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, nesciunt! Porro excepturi fugit dolore rem sit ullam aspernatur, cupiditate laborum similique consequuntur maiores culpa nostrum rerum! Voluptatibus facere totam, maiores sit deserunt, obcaecati molestias quisquam, accusamus earum ipsum similique veniam eum ut? Ea beatae sint voluptas voluptate in ipsum deserunt?"
-        },
-        {
             name: "Rhei Marc Perez",
             relation: "Classmate",
+            createdAt: moment("2025-04-26 20:55:10").toDate(),
+            value: "The way you've showcased your projects is engaging, and it's clear you've put a lot of thought into creating an effective portfolio!"
+        },
+        {
+            name: "Rean Ariban",
+            relation: "Classmate",
+            createdAt: moment("2025-04-21 08:59:11").toDate(),
+            value: "Your content is well-presented, and I love how you've highlighted your projects and skills – it's easy to see your strengths!"
+        },
+        {
+            name: "Jessica Santos",
+            relation: "Classmate",
+            createdAt: moment("2025-02-20 16:14:01").toDate(),
+            value: "Your attention to detail in design elements is impressive, and the overall aesthetic is visually stunning!"
+        },
+        {
+            name: "Liza Rellosa",
+            relation: "Classmate",
+            createdAt: moment("2025-02-12 14:42:12").toDate(),
+            value: "Your web portfolio strikes a perfect balance between creativity and functionality! The design is engaging, and the layout is well-organized, making it easy to explore your work. Keep up the fantastic work!"
+        },
+        {
+            name: "John Carlo Amparado",
+            relation: "Classmate",
             createdAt: moment("2025-01-14 18:13:59").toDate(),
-            value: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, nesciunt! Porro excepturi fugit dolore rem sit ullam aspernatur, cupiditate laborum similique consequuntur maiores culpa nostrum rerum! Voluptatibus facere totam, maiores sit deserunt, obcaecati molestias quisquam, accusamus earum ipsum similique veniam eum ut? Ea beatae sint voluptas voluptate in ipsum deserunt?"
+            value: "Your web portfolio boasts a clean design that's easy to navigate. You've effectively showcased your work, and the layout is logical and intuitive."
         },
     ]
 }
